23c751b929eaaa98b2466d03395ae91a5cda9e09
Matej2405 <mbrodarac@student.uniri.hr>
2025-11-05 19:57:10 +0100

    Dockerized the application

---
+++ SocialMediaApp.Web/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMediaApp.Web.Controllers
+{
+    [ApiController]
+    [Route("api/health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHealthStatus() => Ok(
+            new
+            {
+                status = "Healthy",
+                timestamp = System.DateTime.UtcNow
+            });
+    }
+}
